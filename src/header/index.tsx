884ee5820b821c04c1ba05045cafc385c82e98a4
iwilson001 <iwilson001@csbsju.edu>
2023-07-04 14:45:17 -0400

    Created folder for components, started styling header

---
+++ src/header/index.tsx
@@ -0,0 +1,10 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="wrapper">
+      <div className="calc">calc</div>
+      <div className="theme-swap">(THEMES HERE)</div>
+    </div>
+  );
+}
